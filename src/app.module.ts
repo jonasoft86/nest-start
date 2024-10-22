6927a49e0142cdbfb6f829018e1de915b4b066c9
Jonasoft <jonathan.monroy@live.com>
2024-10-22 18:50:30 -0300

    Brands,Seed data

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { CarsController } from './cars/cars.controller';
 import { CarsService } from './cars/cars.service';
+import { BrandsModule } from './brands/brands.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule, SeedModule],
   controllers: [CarsController],
   providers: [CarsService],
 })
